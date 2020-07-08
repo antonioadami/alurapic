3486b73643deb69d7be36027057268ba6ab61e86
Antonio Adami <antonioadami@gmail.com>
2020-07-08 13:02:21 -0300

    Fim aula6-parte1

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,9 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
-    declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent ],
+    declarations: [ 
+        PhotoComponent, 
+        PhotoListComponent, 
+        PhotoFormComponent, 
+        PhotosComponent,
+        FilterByDescription,
+        LoadButtonComponent,
+    ],
     imports: [CommonModule, HttpClientModule]
 })
 export class PhotosModule {
