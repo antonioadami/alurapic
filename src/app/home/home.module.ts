277b1918d0529539e0bbf4956f89270a30ab17c4
Antonio Adami <antonioadami@gmail.com>
2020-07-09 12:05:46 -0300

    Parte2-aula1 | Página de login

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SignInComponent } from './signin/signin.component';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+
+@NgModule({
+    declarations: [ SignInComponent ],
+    imports: [ 
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        RouterModule
+    ]
+})
+export class HomeModule{}
